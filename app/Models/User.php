c928f611e10a6ff34f04ae5ddea22a279792e40b
ganddi <kooganddi@gmail.com>
2025-05-12 01:24:29 +0800

    update model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,17 +2,47 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    protected $fillable = ['name', 'email', 'password', 'role'];
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
-    protected $casts = [
-        'role' => 'string', // ENUM: admin, penyewa, owner
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable =  ['name', 'email', 'password', 'role'];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
     ];
 
-    // Relasi One-to-Many dengan rentals
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'role' => 'string', 
+        ];
+    }
+     
     public function rentals()
     {
         return $this->hasMany(Rental::class, 'user_id');
