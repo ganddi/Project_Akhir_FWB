ee0ff7058f49875bd1dc4ecea866ef5c4ebc94e1
ganddi <kooganddi@gmail.com>
2025-05-19 14:59:16 +0800

    intergrasi template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -323,7 +323,7 @@
       <a href="index.html" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">Bootslander</h1>
+        <h1 class="sitename">mudahBerkemah</h1>
       </a>
 
       <nav id="navmenu" class="navmenu">
