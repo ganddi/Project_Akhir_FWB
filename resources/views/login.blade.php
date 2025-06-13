1184055172340e69fa55265fb43ab437b60f0c49
ganddi <kooganddi@gmail.com>
2025-06-14 00:23:50 +0800

    Auth

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,78 +12,54 @@
     <title>Login</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset('sb_admin')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('sb_admin') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{asset('sb_admin')}}/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{ asset('sb_admin') }}/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
-<body class="bg-gradient-primary">
-
-    <div class="container">
-
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form class="user" action="{{route('login.submit')}}" method="post">
-                                        @csrf
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user"
-                                                placeholder="Enter Username" name="name">
-                                        </div>
-                                        <div class="form-group">
-                                           <input type="password" class="form-control form-control-user"
-                                                placeholder="Enter password" name="password">
-                                        </div>
-                                        <button type="submit" class="btn-primary btn-user btn-block">
-                                            Login
-                                        </button>
-                                        <hr>
-                                        @if(session('failed'))
-                                            <p>{{session('failed')}}</p>
-                                        @endif
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="/register">Create an Account!</a>
-                                    </div>
-                                </div>
-                            </div>
+<body class="bg-gradient-primary d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="col-lg-4">
+        <div class="card o-hidden border-0 shadow-lg">
+            <div class="card-body p-0">
+                <div class="p-5 text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <form class="user" action="{{ route('login.submit') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <input type="text" class="form-control form-control-user" placeholder="Enter Username"
+                                name="name">
                         </div>
-                    </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control form-control-user" placeholder="Enter password"
+                                name="password">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                        <hr>
+                        @if (session('failed'))
+                            <p class="text-danger">{{ session('failed') }}</p>
+                        @endif
+                    </form>
+                    <hr>
+                    <a class="small" href="/register">Create an Account!</a>
                 </div>
-
             </div>
-
         </div>
-
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{asset('sb_admin')}}/vendor/jquery/jquery.min.js"></script>
-    <script src="{{asset('sb_admin')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('sb_admin') }}/vendor/jquery/jquery.min.js"></script>
+    <script src="{{ asset('sb_admin') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{asset('sb_admin')}}/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ asset('sb_admin') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{asset('sb_admin')}}/js/sb-admin-2.min.js"></script>
+    <script src="{{ asset('sb_admin') }}/js/sb-admin-2.min.js"></script>
 
 </body>
 
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,20 +12,19 @@
     <title>SB Admin 2 - Register</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset('sb_admin')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('sb_admin') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{asset('sb_admin')}}/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{ asset('sb_admin') }}/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-gradient-primary d-flex justify-content-center align-items-center" style="height: 100vh;">
 
     <div class="container">
-
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
@@ -36,27 +35,27 @@
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                             </div>
-                            <form action="{{route('register.submit')}}" class="user" method="POST" >
+                            <form action="{{ route('register.submit') }}" class="user" method="POST">
                                 @csrf
-                                 <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                            placeholder="Username" name="name">
-                                    </div>
-                                
                                 <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
+                                    <input type="text" class="form-control form-control-user" placeholder="Username"
+                                        name="name">
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user"
                                         placeholder="Email Address" name="email">
                                 </div>
-                                 <div class="form-group">
-                                        <input type="password" class="form-control form-control-user"
-                                            id="exampleInputPassword" placeholder="Password" name="password">
-                                    
+
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-user" placeholder="Password"
+                                        name="password">
                                 </div>
+
                                 <button type="submit" class="btn btn-primary btn-user btn-block">
                                     Submit
                                 </button>
                                 <hr>
-
                             </form>
                             <hr>
                             <div class="text-center">
@@ -67,18 +66,17 @@
                 </div>
             </div>
         </div>
-
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{asset('sb_admin')}}/vendor/jquery/jquery.min.js"></script>
-    <script src="{{asset('sb_admin')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('sb_admin') }}/vendor/jquery/jquery.min.js"></script>
+    <script src="{{ asset('sb_admin') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{asset('sb_admin')}}/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ asset('sb_admin') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{asset('sb_admin')}}/js/sb-admin-2.min.js"></script>
+    <script src="{{ asset('sb_admin') }}/js/sb-admin-2.min.js"></script>
 
 </body>
 
