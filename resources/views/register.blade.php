fc18a65eed484badc9de798fdfa493af319f0db9
ganddi <kooganddi@gmail.com>
2025-06-15 12:46:29 +0800

    Auth

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Register</title>
+    <title>Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('sb_admin') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -23,45 +23,38 @@
 </head>
 
 <body class="bg-gradient-primary d-flex justify-content-center align-items-center" style="height: 100vh;">
-
-    <div class="container">
-        <div class="card o-hidden border-0 shadow-lg my-5">
+    <div class="col-lg-4">
+        <div class="card o-hidden border-0 shadow-lg">
             <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
-                <div class="row">
-                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                    <div class="col-lg-7">
-                        <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
-                            </div>
-                            <form action="{{ route('register.submit') }}" class="user" method="POST">
-                                @csrf
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" placeholder="Username"
-                                        name="name">
-                                </div>
-
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-user"
-                                        placeholder="Email Address" name="email">
-                                </div>
-
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-user" placeholder="Password"
-                                        name="password">
-                                </div>
-
-                                <button type="submit" class="btn btn-primary btn-user btn-block">
-                                    Submit
-                                </button>
-                                <hr>
-                            </form>
-                            <hr>
-                            <div class="text-center">
-                                <a class="small" href="/login">Already have an account? Login!</a>
-                            </div>
+                <div class="p-5 text-center">
+                    <div class="text-center">
+                        <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                    </div>
+                    <form action="{{ route('register.submit') }}" class="user" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <input type="text" class="form-control form-control-user" placeholder="Username"
+                                name="name">
                         </div>
+
+                        <div class="form-group">
+                            <input type="email" class="form-control form-control-user" placeholder="Email Address"
+                                name="email">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="password" class="form-control form-control-user" placeholder="Password"
+                                name="password">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                            Submit
+                        </button>
+                        <hr>
+                    </form>
+                    <hr>
+                    <div class="text-center">
+                        <a class="small" href="/login">Already have an account? Login!</a>
                     </div>
                 </div>
             </div>
