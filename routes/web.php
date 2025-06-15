4168b601d82f60ee68e754279b6061c9e393bf84
ganddi <kooganddi@gmail.com>
2025-06-15 12:46:41 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use App\Http\Controllers\admin;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\guest;
 use App\Http\Controllers\middleware;
 use App\Http\Controllers\pemilik;
 use App\Http\Controllers\penyewa;
 use App\Http\Controllers\ProfileController;
 use App\Models\Item;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/',[guest::class,'welcome'])->name('welcome');
 
 Route::middleware('auth','role:admin')->group(function(){
     route::get('/dashboard',[admin::class,'dashboard'])->name('dashboard');
@@ -16,7 +20,17 @@ Route::middleware('auth','role:admin')->group(function(){
     route::match(['get', 'post'], '/editItem/{id}', [admin::class, 'editItem'])->name('editItem');
     Route::post('/simpanItem', [admin::class, 'simpanItem'])->name('simpanItem');
     Route::post('/hapus/{id}', [admin::class, 'hapus'])->name('hapus');
+
+
     Route::get('/lihatUser', [admin::class, 'lihatUser'])->name('lihatUser');
+    
+    
+    Route::get('/belum_dibayar', [admin::class, 'belum_dibayar'])->name('belum_dibayar');
+    Route::post('/belum_dibayar/konfirmasi/{id}', [admin::class, 'bayar'])->name('bayar');
+    Route::get('/diPinjam', [admin::class, 'diPinjam'])->name('diPinjam');
+    Route::post('/diPinjam/konfirmasi/{id}', [admin::class, 'kembali'])->name('kembali');
+    Route::get('/diKembalikan', [admin::class, 'diKembalikan'])->name('diKembalikan');
+    
 });
 
 Route::middleware('auth','role:pemilik')->group(function(){
@@ -26,18 +40,12 @@ Route::middleware('auth','role:pemilik')->group(function(){
 
 Route::middleware('auth','role:penyewa')->group(function(){
     route::get('/penyewa',[penyewa::class,'penyewa'])->name('penyewa');
+    route::post('/penyewa/submit',[penyewa::class,'rental'])->name('submitRental');
 });
 
 Route::middleware('auth')->group(function(){
     route::post('/logout',[AuthController::class,'Logout'])->name('logout');
 });
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 
 route::get('/register',[AuthController::class,'registerLihat'])->name('register.lihat');
 route::post('/register/submit',[AuthController::class,'registerSubmit'])->name('register.submit');
