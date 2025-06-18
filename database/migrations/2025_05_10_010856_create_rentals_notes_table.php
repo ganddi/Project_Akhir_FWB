776bea44ec132dc7c013f9c01a7227072ccce471
ganddi <kooganddi@gmail.com>
2025-06-18 10:11:08 +0800

    migrasi

--- database/migrations/2025_05_10_010856_create_rentals_notes_table.php
+++ database/migrations/2025_05_10_010856_create_rentals_notes_table.php
@@ -10,32 +10,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('rentals_notes', function (Blueprint $table) {
-            $table->unsignedBigInteger('rental_id')->primary();
+            $table->id();
+            $table->foreignId('rental_id')->unique()->constrained('rentals')->onDelete('cascade');
             $table->text('notes')->nullable();
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-
-            $table->foreign('rental_id')->references('id')->on('rentals')->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at');
         });
-
-        DB::unprepared('
-            CREATE TRIGGER before_insert_rentals_notes
-            BEFORE INSERT ON rentals_notes
-            FOR EACH ROW
-            BEGIN
-                DECLARE rental_status ENUM("belum_dibayar", "dipinjam", "dikembalikan");
-
-                SELECT rental_status INTO rental_status FROM rentals WHERE id = NEW.rental_id;
-                IF rental_status != "dikembalikan" THEN
-                    SIGNAL SQLSTATE "45000"
-                    SET MESSAGE_TEXT = "Catatan hanya dapat dibuat untuk peminjaman dengan status dikembalikan";
-                END IF;
-            END
-        ');
     }
 
     public function down(): void
     {
-        DB::unprepared('DROP TRIGGER IF EXISTS before_insert_rentals_notes');
         Schema::dropIfExists('rentals_notes');
     }
 };
